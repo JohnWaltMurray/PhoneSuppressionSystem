d48a45dc2ff7f58883b10e846bd6fe76dcdd7cbe
Unknown <johnwaltmurray@gmail.com>
2018-09-25 15:48:15 -0400

    Basic table functionality

--- app/app.js
+++ app/app.js
@@ -25,3 +25,20 @@ function changePageTitle(page_title){
     // change title tag
     document.title=page_title;
 }
+
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
