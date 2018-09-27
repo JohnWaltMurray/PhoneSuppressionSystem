48310ae3e1dd702bf3be28e8ecce8e68bb3a1d5c
Unknown <johnwaltmurray@gmail.com>
2018-09-27 16:19:19 -0400

    Search functionality, delete functionality

--- app/table/entries.js
+++ app/table/entries.js
@@ -1,26 +1,7 @@
-$(document).ready(function(){
-    
-    showTable();
-    
-});
-function showTable() {
+function readAllEntries(data, keywords) {
     //$.getJSON("http://localhost/api/table/read.php", function(data){
         var read_data_html="";
 
-        //Search button
-        read_data_html+="<form id='search-form' action='#' method='post'>";
-            read_data_html+="<div class='input-group pull-left w-30-pct'>";
- 
-                read_data_html+="<input type='text' name='keywords' class='form-control search-keywords' placeholder='Search...' />";
-        
-                read_data_html+="<span class='input-group-btn'>";
-                    read_data_html+="<button type='submit' class='btn btn-default' type='button'>";
-                        read_data_html+="<span class='glyphicon glyphicon-search'></span>";
-                    read_data_html+="</button>";
-                read_data_html+="</span>";
- 
-            read_data_html+="</div>";
-        read_data_html+="</form>";
 
         read_data_html+="<div id='create-row' class='btn btn-primary pull-right m-b-15px create-entry-button'>";
             read_data_html+="<span class='glyphicon glyphicon-plus'></span> Create Row";
@@ -61,16 +42,20 @@ function showTable() {
                     read_data_html+="<td>";
                         read_data_html+="<span>test data lmao</span>";
                     read_data_html+="</td>";
+
                     // 'action' buttons
                     read_data_html+="<td>";
+                        // read one button
                         read_data_html+="<button class='btn btn-primary m-r-10px read-one-button'>";
                             read_data_html+="<span class='glyphicon glyphicon-eye-open'></span> Read";
                         read_data_html+="</button>";
             
+                        // edit button
                         read_data_html+="<button class='btn btn-info m-r-10px update-button'>";
                             read_data_html+="<span class='glyphicon glyphicon-edit'></span> Edit";
                         read_data_html+="</button>";
             
+                        // delete button
                         read_data_html+="<button class='btn btn-danger delete-entry-button'>";
                             read_data_html+="<span class='glyphicon glyphicon-remove'></span> Delete";
                         read_data_html+="</button>";
@@ -82,7 +67,4 @@ function showTable() {
         // end table
         read_data_html+="</table>";
         $("#page-content").html(read_data_html);
-        changePageTitle("Phone Suppression Table");
-
-   // });
 }
--- app/table/read-one.js
+++ app/table/read-one.js
@@ -2,35 +2,30 @@ $(document).ready(function(){
 
     $(document).on('click', 'read-one-button', function(){
         var id = $(this).attr('data-id');
-        $.getJSON("http://localhost/api/product/read_one.php?id=" + id, function(data){
+        $.getJSON("http://localhost/api/contact/read_one.php?id=" + id, function(data){
             var read_one_html="";
  
-            // when clicked, it will show the product's list
-            read_one_html+="<div id='read-products' class='btn btn-primary pull-right m-b-15px go-back-button'>";
+            read_one_html+="<div id='read-entries' class='btn btn-primary pull-right m-b-15px go-back-button'>";
                 read_one_html+="<span class='glyphicon glyphicon-list'></span> Go Back";
             read_one_html+="</div>";
 
             read_one_html+="<table class='table table-bordered table-hover'>";
  
-            // product name
             read_one_html+="<tr>";
                 read_one_html+="<td class='w-30-pct'>ID</td>";
                 read_one_html+="<td class='w-70-pct'>" + data.id + "</td>";
             read_one_html+="</tr>";
-        
-            // product price
+
             read_one_html+="<tr>";
                 read_one_html+="<td>Phone Number</td>";
                 read_one_html+="<td>" + data.number + "</td>";
             read_one_html+="</tr>";
         
-            // product description
             read_one_html+="<tr>";
                 read_one_html+="<td>First Name</td>";
                 read_one_html+="<td>" + data.fname + "</td>";
             read_one_html+="</tr>";
         
-            // product category name
             read_one_html+="<tr>";
                 read_one_html+="<td>Last Name</td>";
                 read_one_html+="<td>" + data.lname + "</td>";
